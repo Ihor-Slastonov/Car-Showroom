6c9b5d1fd47729c95aeb0a11ef9ed989429f5abd
Jam <104766756+Ihor-Slastonov@users.noreply.github.com>
2024-09-22 12:29:23 +0300

    Add image carousel

---
+++ src/components/VehicleFullDetails/VehicleImagesCarousel/VehicleImagesCarousel.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import { Navigation, Thumbs, Pagination } from 'swiper/modules';
+
+import MyImage from '../../common/MyImage/MyImage';
+
+import {
+  CarouselContainer,
+  StyledSwipper,
+  StyledSlide,
+  ThumbsSwiper,
+  ThumbsSlide,
+} from './VehicleImagesCarousel.styled';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/thumbs';
+
+const VehicleImagesCarousel = ({ images }) => {
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+
+  return (
+    <CarouselContainer>
+      <StyledSwipper
+        modules={[Navigation, Pagination, Thumbs]}
+        spaceBetween={10}
+        navigation={true}
+        pagination={{ clickable: true }}
+        thumbs={thumbsSwiper ? { swiper: thumbsSwiper } : undefined}
+      >
+        {images?.map((image, idx) => (
+          <StyledSlide key={idx}>
+            <MyImage src={image} alt={`vehicle-image-${idx}`} />
+          </StyledSlide>
+        ))}
+      </StyledSwipper>
+
+      <ThumbsSwiper
+        modules={[Thumbs]}
+        onSwiper={setThumbsSwiper}
+        spaceBetween={10}
+        freeMode={true}
+        watchSlidesProgress={true}
+        slidesPerView={3}
+      >
+        {images?.map((image, idx) => (
+          <ThumbsSlide key={idx}>
+            <MyImage
+              src={image}
+              alt={`vehicle-thumbnail-${idx}`}
+              loaderSize={10}
+            />
+          </ThumbsSlide>
+        ))}
+      </ThumbsSwiper>
+    </CarouselContainer>
+  );
+};
+
+export default VehicleImagesCarousel;
+
+VehicleImagesCarousel.propTypes = {
+  images: PropTypes.array,
+};
---
+++ src/components/VehicleFullDetails/VehicleImagesCarousel/VehicleImagesCarousel.styled.js
@@ -0,0 +1,67 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import styled from 'styled-components';
+
+export const CarouselContainer = styled.div`
+  width: 100%;
+
+  border: 1px solid ${({ theme }) => theme.colors.gold};
+  background-color: ${({ theme }) => theme.colors.dark};
+
+  ${({ theme }) => theme.breakpoints.desktop} {
+    width: 600px;
+  }
+`;
+
+export const StyledSwipper = styled(Swiper)`
+  width: 100%;
+  height: 250px;
+
+  .swiper-button-prev,
+  .swiper-button-next {
+    color: ${({ theme }) => theme.colors.gold}; /* Цвет навигационных стрелок */
+  }
+
+  .swiper-pagination-bullet {
+    background-color: ${({ theme }) => theme.colors.gold};
+  }
+
+  ${({ theme }) => theme.breakpoints.desktop}{
+    height: 274px;
+  }
+`;
+
+export const StyledSlide = styled(SwiperSlide)`
+  & > img {
+    object-fit: contain;
+  }
+`;
+
+export const ThumbsSwiper = styled(Swiper)`
+  margin-top: 20px;
+  width: 100%;
+  height: 60px;
+  padding: 4px 0;
+
+  border-top: 1px solid ${({ theme }) => theme.colors.gold};
+
+  ${({ theme }) => theme.breakpoints.tablet} {
+    height: 80px;
+  }
+  ${({ theme }) => theme.breakpoints.desktop} {
+    height: 120px;
+  }
+
+  .swiper-slide-thumb-active {
+    border: 1px solid ${({ theme }) => theme.colors.gold};
+    box-shadow: 0 0 10px rgba(32, 32, 20, 0.717);
+  }
+`;
+
+export const ThumbsSlide = styled(SwiperSlide)`
+  cursor: pointer;
+
+  & > img {
+    object-fit: cover;
+  }
+`;
